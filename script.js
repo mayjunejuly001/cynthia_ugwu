dd22c455f629174b4b4804175634b29437833c20
mayjunejuly001 <mayankvermacod@gmail.com>
2023-11-28 17:09:58 +0530

    third

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ const loco = new LocomotiveScroll({
   smooth: true,
 })
 
+var timeout ;
+
+function circleChaptaKaro() {
+  //define default scale value
+  var xscale = 1
+  var yscale = 1
+
+  var xprev = 0
+  var yprev = 0
+
+  window.addEventListener('mousemove', function (dets) {
+    clearTimeout(timeout)
+    // var xdiff = dets.clientX - xprev
+    // var ydiff = dets.clientX - xprev
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev)
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev)
+    xprev = dets.clientX
+    yprev = dets.clientY
+
+    circleMouseFollower(xscale, yscale)
+    
+    timeout = setTimeout(function () {
+      document.querySelector(
+      '#minicircle'
+      ).style.transform = `translate(${dets.clientX}px , ${dets.clientY}px) scale(1,1)`
+    },100)
+    
+  })
+}
+circleChaptaKaro()
 function FirstPageElem() {
   var tl = gsap.timeline()
 
@@ -11,27 +41,26 @@ function FirstPageElem() {
     opacity: 0,
     duration: 1,
     ease: Expo.easeInOut,
-  })
-  .to('.boundingelem', {
+  }).to('.boundingelem', {
     y: '-10',
     ease: Expo.easeInOut,
     duration: 2,
     stagger: 0.2,
   })
   tl.from('#herofooter', {
-    y:-10, 
-    opacity:0,
-    duration:-1.5,
-    delay:-1,
-    ease: Expo.easeInOut
+    y: -10,
+    opacity: 0,
+    duration: -1.5,
+    delay: -1,
+    ease: Expo.easeInOut,
   })
 }
 
-function circleMouseFollower() {
+function circleMouseFollower(xscale, yscale) {
   window.addEventListener('mousemove', function (dets) {
     document.querySelector(
       '#minicircle'
-    ).style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`
+    ).style.transform = `translate(${dets.clientX}px , ${dets.clientY}px) scale(${xscale}, ${yscale})`
   })
 }
 
